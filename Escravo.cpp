9655a66832aa5ba2ca5c2beb939687102512600e
Bryan Willes <bryan.corcetti@gmail.com>
2020-11-27 00:11:40 -0300

    Adicionada novas mensagens para serem exibidas.

--- Escravo.cpp
+++ Escravo.cpp
@@ -1,11 +1,9 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
 
-
 #define SRCADDR 0x02
 
-int cafe = 0;
-bool cafeOff = false;
+byte cafe = 0;
 LiquidCrystal lcd(
   12, 11, 10, 7, 6, 5, 4
 );
@@ -22,26 +20,37 @@ void setup()
 
 void Console(int lendo){
   cafe = Wire.read();
-  if(cafe == 1){
-    lcd.clear();
-    lcd.print("Preparando o seu");
-    lcd.setCursor(0, 1);
-    lcd.print("cafe...");
-  }
-  if(cafe == 2){
-    lcd.clear();
-    lcd.print("Misturando...");
-  }
-  if(cafe == 3){
-    lcd.clear();
-    lcd.print("O seu cafe esta");
-    lcd.setCursor(0, 1);
-    lcd.print("pronto.");
-  }if(cafe == 4){
-    lcd.clear();
-    lcd.print("Cafe acabou.");
-  }if(cafe == 5){
-    setup();
+  switch(cafe){
+    case 1 :
+    	lcd.clear();
+      	lcd.print("Preparando o seu");
+      	lcd.setCursor(0, 1);
+      	lcd.print("cafe...");
+    	break;
+    case 2 :
+    	lcd.clear();
+    	lcd.print("Misturando...");
+    	break;
+    case 3 :
+    	lcd.clear();
+   		lcd.print("O seu cafe esta");
+    	lcd.setCursor(0, 1);
+    	lcd.print("pronto.");
+    	break;
+    case 4 :
+    	lcd.clear();
+    	lcd.print("Cafe acabou.");
+    	break;
+    case 5 :
+    	setup();
+    	break;
+    case 6 : 
+    	lcd.clear();
+   		lcd.print("Coloque um copo");
+    	lcd.setCursor(0, 1);
+    	lcd.print("e aperte de novo");
+    default : 
+    	false;
   }
   
 }
