b52aeb8080036138040661edfb4479fb359d6797
unknown <bryan.corcetti@gmail.com>
2020-11-27 12:41:43 -0300

    Consertado aviso de copo ausente.

--- Escravo.cpp
+++ Escravo.cpp
@@ -5,58 +5,64 @@
 
 byte cafe = 0;
 LiquidCrystal lcd(
-  12, 11, 10, 7, 6, 5, 4
-);
+	12, 11, 10, 7, 6, 5, 4);
 
 void setup()
 {
-  Wire.begin(SRCADDR);
-  Wire.onReceive(Console);
-  Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.clear();
-  lcd.print("Aceita um cafe?");
+	Wire.begin(SRCADDR);
+	Wire.onReceive(Console);
+	Serial.begin(9600);
+	lcd.begin(16, 2);
+	lcd.clear();
+	lcd.print("Aceita um cafe?");
 }
 
-void Console(int lendo){
-  cafe = Wire.read();
-  switch(cafe){
-    case 1 :
-    	lcd.clear();
-      	lcd.print("Preparando o seu");
-      	lcd.setCursor(0, 1);
-      	lcd.print("cafe...");
-    	break;
-    case 2 :
-    	lcd.clear();
-    	lcd.print("Misturando...");
-    	break;
-    case 3 :
-    	lcd.clear();
-   		lcd.print("O seu cafe esta");
-    	lcd.setCursor(0, 1);
-    	lcd.print("pronto.");
-    	break;
-    case 4 :
-    	lcd.clear();
-    	lcd.print("Cafe acabou.");
-    	break;
-    case 5 :
-    	setup();
-    	break;
-    case 6 : 
-    	lcd.clear();
-   		lcd.print("Coloque um copo");
-    	lcd.setCursor(0, 1);
-    	lcd.print("e aperte de novo");
-    default : 
-    	false;
-  }
-  
+void Console(int lendo)
+{
+	cafe = Wire.read();
+	switch (cafe)
+	{
+	case 1:
+		lcd.clear();
+		lcd.print("Preparando o seu");
+		lcd.setCursor(0, 1);
+		lcd.print("cafe...");
+		break;
+	case 2:
+		lcd.clear();
+		lcd.print("Misturando...");
+		break;
+	case 3:
+		lcd.clear();
+		lcd.print("O seu cafe esta");
+		lcd.setCursor(0, 1);
+		lcd.print("pronto.");
+		break;
+	case 4:
+		lcd.clear();
+		lcd.print("Cafe acabou.");
+		break;
+	case 5:
+		setup();
+		break;
+	case 6:
+		lcd.clear();
+		lcd.print("Coloque um copo");
+		lcd.setCursor(0, 1);
+		lcd.print("e aperte de novo");
+		break;
+	case 7:
+		lcd.clear();
+		lcd.print("Preparando o seu");
+		lcd.setCursor(0, 1);
+		lcd.print("cafe com leite..");
+		break;
+	default:
+		false;
+	}
 }
-  
 
 void loop()
 {
-  delay(100);
+	delay(100);
 }
--- Mestre.cpp
+++ Mestre.cpp
@@ -1,7 +1,8 @@
 #include <Wire.h>
 
 #define LED 2
-#define BTN 4
+#define BTNC 4
+#define BTNL 3
 #define BIP 8
 #define SRCADDR 0x01
 #define DSTADDR 0x02
@@ -19,30 +20,31 @@ bool STADEBTN = false;
 void setup()
 {
   pinMode(LED, OUTPUT);
-  pinMode(BTN, INPUT);
+  pinMode(BTNC, INPUT);
+  pinMode(BTNL, INPUT);
   pinMode(BIP, OUTPUT);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   Serial.begin(9600);
   Wire.begin(SRCADDR);
-  attachInterrupt(COPO, TemCopo, CHANGE);
 }
 
 void Distancia()
 {
   TEMPOECHO = pulseIn(ECHO, HIGH);
-  DISTANCIA = (((TEMPOECHO*TEMPO)/2)*100);
+  DISTANCIA = (((TEMPOECHO * TEMPO) / 2) * 100);
   Serial.println(COPO);
-  if(DISTANCIA < 6){
+  if (DISTANCIA < 6)
+  {
     COPO = true;
+  }
+  else
+  {
+    COPO = false;
   }
   delay(100);
 }
 
-void TemCopo()
-{
-  Serial.println("Copo");
-}
 void Disparo()
 {
   digitalWrite(TRIG, HIGH);
@@ -50,21 +52,31 @@ void Disparo()
   digitalWrite(TRIG, LOW);
 }
 
-void Vazio(){
-   Wire.beginTransmission(DSTADDR);
-   Wire.write(4);
-   Wire.endTransmission();
-   Serial.println("Tanque de agua vazio");
+void Vazio()
+{
+  Wire.beginTransmission(DSTADDR);
+  Wire.write(4);
+  Wire.endTransmission();
+  Serial.println("Tanque de agua vazio");
 }
 
 void CafeLeite()
 {
   Wire.beginTransmission(DSTADDR);
-  Wire.write(1);
+  Wire.write(7);
   Wire.endTransmission();
   Serial.println("Colocando po de cafe");
   delay(2000);
   Serial.println("Colocando leite em po");
+  delay(2000);
+}
+
+void Cafe()
+{
+  Wire.beginTransmission(DSTADDR);
+  Wire.write(1);
+  Wire.endTransmission();
+  Serial.println("Colocando po de cafe");
   delay(2000);
 }
 
@@ -84,51 +96,86 @@ void Pronto()
   Wire.endTransmission();
   tone(BIP, FREQ, 3);
   Serial.println("Cafe com leite pronto.");
-  Disparo();
-  Distancia();
-  if(!COPO){
+  while (COPO)
+  {
+    Disparo();
+    Distancia();
+    delay(100);
+  }
+  if (!COPO)
+  {
     Wire.beginTransmission(DSTADDR);
     Wire.write(5);
     Wire.endTransmission();
   }
 }
 
+void Cont()
+{
+  if (CONT < 2)
+  {
+    delay(1000);
+    Wire.beginTransmission(DSTADDR);
+    Wire.write(5);
+    Wire.endTransmission();
+  }
+
+  if (CONT == 2)
+  {
+    delay(1000);
+    Vazio();
+  }
+
+  delay(500);
+}
+
 void loop()
 {
-  
-  while(CONT < 2){
-      if(digitalRead(BTN) == HIGH){
-        delay(100);
-        Disparo();
-        Distancia();
-        if(!COPO){
-          Wire.beginTransmission(DSTADDR);
-          Wire.write(6);
-          Wire.endTransmission();
-        }
-        if(COPO){
-          CONT++;
-          CafeLeite();
-          Agua();
-          Pronto();
 
-          if(CONT < 2){
-            delay(1000);
-            Wire.beginTransmission(DSTADDR);
-            Wire.write(5);
-            Wire.endTransmission();
-          }
-
-          if(CONT == 2){
-            delay(1000);
-            Vazio();
-          }
-
-            delay(500);
-        }
+  while (CONT < 2)
+  {
+    if (digitalRead(BTNC) == HIGH)
+    {
+      delay(100);
+      Disparo();
+      Distancia();
+      if (!COPO)
+      {
+        Wire.beginTransmission(DSTADDR);
+        Wire.write(6);
+        Wire.endTransmission();
+      }
+      if (COPO)
+      {
+        CONT++;
+        Cafe();
+        Agua();
+        Pronto();
+        Cont();
       }
     }
-    
-      
+
+    if (digitalRead(BTNL) == HIGH)
+    {
+      delay(100);
+      Disparo();
+      Distancia();
+      if (!COPO)
+      {
+        Wire.beginTransmission(DSTADDR);
+        Wire.write(6);
+        Wire.endTransmission();
+      }
+      if (COPO)
+      {
+        CONT++;
+        CafeLeite();
+        Agua();
+        Pronto();
+        Cont();
+      }
+    }
+  }
+
   digitalWrite(LED, HIGH);
 }
